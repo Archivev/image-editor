084eac52db96ccdd171d613e543b11891e115057
zkm <302866617@qq.com>
2024-05-30 22:00:12 +0800

    去除不必要的操作按钮

--- lib/tools.tsx
+++ lib/tools.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import { forwardRef, useState } from 'react';
-import Popover, { ArrowContainer } from 'react-tiny-popover';
-import cx from 'classnames';
 
 import { useCombinedRefs } from './utils';
-import Stroke from './Stroke';
 import Confirm from './confirm.svg';
-import Back from './rollback.svg';
 import DownLoad from './download.svg';
 import Close from './close.svg';
 
@@ -44,137 +40,6 @@ const Tools = forwardRef<HTMLDivElement, IProps>(
 
     return (
       <div className="tools" ref={combinedRef}>
-        <Popover
-          position={position}
-          align="start"
-          isOpen={selected === 'rect'}
-          content={({ position, targetRect, popoverRect }) => (
-            <ArrowContainer
-              position={position}
-              targetRect={targetRect}
-              popoverRect={popoverRect}
-              arrowColor={'#272323'}
-              arrowSize={5}
-            >
-              <Stroke
-                info={info}
-                onChange={options =>
-                  setInfo((opt: { size?: number; color?: string }) => ({
-                    ...opt,
-                    ...options
-                  }))
-                }
-              />
-            </ArrowContainer>
-          )}
-        >
-          <div
-            className={cx('tools-item tools-rect', {
-              'tool-selected': type === 'rect'
-            })}
-            onClick={() => handleClick('rect')}
-          />
-        </Popover>
-        <Popover
-          position={position}
-          align="start"
-          isOpen={selected === 'circle'}
-          content={({ position, targetRect, popoverRect }) => (
-            <ArrowContainer
-              position={position}
-              targetRect={targetRect}
-              popoverRect={popoverRect}
-              arrowColor={'#272323'}
-              arrowSize={5}
-            >
-              <Stroke
-                info={info}
-                onChange={options =>
-                  setInfo((opt: { size?: number; color?: string }) => ({
-                    ...opt,
-                    ...options
-                  }))
-                }
-              />
-            </ArrowContainer>
-          )}
-        >
-          <div
-            className={cx('tools-item tools-circle', {
-              'tool-selected': type === 'circle'
-            })}
-            onClick={() => handleClick('circle')}
-          />
-        </Popover>
-        <Popover
-          position={position}
-          align="start"
-          isOpen={selected === 'line'}
-          content={({ position, targetRect, popoverRect }) => (
-            <ArrowContainer
-              position={position}
-              targetRect={targetRect}
-              popoverRect={popoverRect}
-              arrowColor={'#272323'}
-              arrowSize={5}
-            >
-              <Stroke
-                info={info}
-                onChange={options =>
-                  setInfo((opt: { size?: number; color?: string }) => ({
-                    ...opt,
-                    ...options
-                  }))
-                }
-              />
-            </ArrowContainer>
-          )}
-        >
-          <div
-            className={cx('tools-item tools-text', {
-              'tool-selected': type === 'line'
-            })}
-            onClick={() => handleClick('line')}
-          >
-            L
-          </div>
-        </Popover>
-        <Popover
-          position={position}
-          align="start"
-          isOpen={selected === 'mosaic'}
-          content={({ position, targetRect, popoverRect }) => (
-            <ArrowContainer
-              position={position}
-              targetRect={targetRect}
-              popoverRect={popoverRect}
-              arrowColor={'#272323'}
-              arrowSize={5}
-            >
-              <Stroke
-                info={info}
-                onChange={options =>
-                  setInfo((opt: { size?: number; color?: string }) => ({
-                    ...opt,
-                    ...options
-                  }))
-                }
-                isHideColor
-              />
-            </ArrowContainer>
-          )}
-        >
-          <div
-            className={cx('tools-item tools-text', {
-              'tool-selected': type === 'mosaic'
-            })}
-            onClick={() => handleClick('mosaic')}
-          >
-            M
-          </div>
-        </Popover>
-        <div className="tools-hold" />
-        <Back onClick={onCancel} />
         <DownLoad onClick={onDownload} />
         <Close onClick={handleClose} />
         <Confirm className="tools-item" onClick={onCopy} />
